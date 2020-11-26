6fa0ac1175ad4c6b62efc4bda4d92511ddb5ef0e
makerjoe <makerjoe@gmail.com>
2020-11-26 20:30:19 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(function(reg) {
+    navigator.serviceWorker.register('/FWmain/service-worker.js', { scope: '/' }).then(function(reg) {
 
     if(reg.installing) {
       console.log('Service worker installing');
